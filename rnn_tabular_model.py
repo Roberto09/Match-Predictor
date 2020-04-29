e011c9906d36b5f984c63b1b44bb9a51c91e211a
roberto09 <roberto.gt1509@gmail.com>
2020-04-29 13:54:50 -0500

    simple model changes and data

--- rnn_tabular_model.py
+++ rnn_tabular_model.py
@@ -8,13 +8,13 @@ class MyTabularModel(Module):
     an exact copy of TabularModel from fastai however it won't do Batchnorm on our input data
     """
     def __init__(self, emb_szs:ListSizes, n_cont:int, out_sz:int, layers:Collection[int], ps:Collection[float]=None,
-                 emb_drop:float=0., y_range:OptRange=None, use_bn:bool=False, bn_final:bool=False):
+                 emb_drop:float=0., y_range:OptRange=None, use_bn:bool=True, bn_final:bool=False):
         super().__init__()
         ps = ifnone(ps, [0]*len(layers))
         ps = listify(ps, layers)
         self.embeds = nn.ModuleList([embedding(ni, nf) for ni,nf in emb_szs])
         self.emb_drop = nn.Dropout(emb_drop)
-#         self.bn_cont = nn.BatchNorm1d(n_cont)
+        self.bn_cont = nn.BatchNorm1d(n_cont)
         n_emb = sum(e.embedding_dim for e in self.embeds)
         self.n_emb,self.n_cont,self.y_range = n_emb,n_cont,y_range
         sizes = self.get_sizes(layers, out_sz)
@@ -34,7 +34,7 @@ class MyTabularModel(Module):
             x = torch.cat(x, 1)
             x = self.emb_drop(x)
         if self.n_cont != 0:
-#             x_cont = self.bn_cont(x_cont)
+            x_cont = self.bn_cont(x_cont)
             x = torch.cat([x, x_cont], 1) if self.n_emb != 0 else x_cont
         x = self.layers(x)
         if self.y_range is not None:
@@ -90,11 +90,6 @@ class RnnTabularModel(nn.Module):
         self.h = h.detach() #Correct 
         
         rnn_res = self.h_o(self.bn(res)) #Correct
-        
-#         print(rnn_res.requires_grad)
-        
-#         print(x_cat.requires_grad)
-#         print(x_cont.requires_grad)
         
         # new part, using results of both teams
         curr_res = []
